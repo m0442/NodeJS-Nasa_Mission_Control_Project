a733ad5b7940110b280ac078b6b4a797bd16b589
Moaaz Afifi <moaazemad2002@gmail.com>
2025-06-03 20:58:12 +0300

    Setting up Mongo DB Atlas

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,29 @@
 const http = require("http");
 
+const mongoose = require("mongoose");
+
 const app = require("./app");
 
 const { loadPlanetsData } = require("./models/planets.model");
+
 const PORT = process.env.PORT || 8000;
+
+const MONGO_URL =
+  "mongodb+srv://nasa-api:R8IS7knQ6OPbxU0R@nasacluster.2ogcshr.mongodb.net/?retryWrites=true&w=majority&appName=NASACluster";
 
 const server = http.createServer(app);
 
+mongoose.connection.once("open", () => {
+  console.log("MobgoDB connection Ready!..");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
+
   await loadPlanetsData();
   server.listen(PORT, () => {
     console.log(`listening on port ${PORT}...`);
